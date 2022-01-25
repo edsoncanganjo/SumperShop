fa433098e92a7ff9958638e8cf26c8af231ef1f4
edsoncanganjo <edsoncanganjo@hotmail.com>
2022-01-25 15:24:32 +0000

    EFC - Updating the Repository

--- SumperShop/Models/RegisterNewUserViewModel.cs
+++ SumperShop/Models/RegisterNewUserViewModel.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
 
-namespace SumperShop.Data.Entities
+namespace SumperShop.Models
 {
-    public class User : IdentityUser
+    public class RegisterNewUserViewModel
     {
-
     }
 }
